bf32f140487e6786cd1b4afc8643de9a1425050f
JoBICHE <jobiche.pro@gmail.com>
2021-12-22 12:35:07 +0100

    1Variables and 2Fonctions files + Corrections folder  added

---
+++ 1Variables.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les variables PHP</title>
+</head>
+<body>
+    
+    <h1>Exercice 1 : Créer puis manipuler des variables en PHP :</h1>
+    
+    <!--     Remplacez les XXX ci-dessous par des valeurs contenues dans des variables.
+    Commencez pour cela par déclarer les variables, puis utilisez les pour insérer leurs valeurs dans le code HTML suivant :  -->
+
+    <p>Bonjour je m'appelle XXX, j'ai XXX ans et je suis XXX de profession.</p>
+
+    <!--     Ensuite, tentez de réstituer un chiffre aléatoire compris entre 1 et 9, qui sera insérer dans le code HTML suivant : -->
+    
+    <p>Mon chiffre porte bonheur est le XXX</p>
+
+    <!-- Multipliez désormais ce chiffre par 20, en le stockant dans un nouvelle variable et en le restituant dans le code HTML suivant :  -->
+
+    <p>Cette information ne présente aucun intéret, mais si vous vous posez la question, sachez que mon chiffre prote bonheur multiplié par 20 donne XXX</p>
+
+
+    
+
+
+    
+    
+</body>
+</html>
---
+++ 2Fonctions.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les fonctions PHP</title>
+</head>
+<body>
+    
+    <!-- Stockez maintentant les deux premières lettres de la variable utilisée pour le prénom dans une variable surnom et restituez la dans le code HTML suivant : -->
+
+    <p>Mon surnom correspond aux deux premières lettres de mon prénom, on me surnomme donc XXX</p>
+
+    <!-- Affichons désormais la date du jour, stockée dans une variable : -->
+    
+    <p>Nous sommes le XXX</p>
+
+    <!-- Anisi que l'heure : -->
+
+    <p>Il est XXX heures</p>
+
+    <!-- Vous avez probablement à ce niveau un résultat du type 'Il est 12:20 heures', ce qui n'est pas très clair... Tentez d'afficher l'heure comme suit :
+    Indice : concaténation -->
+
+    <p>Il est XX heure(s) et XX minute(s).</p>
+</body>
+</html>
