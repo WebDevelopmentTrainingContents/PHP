814741ad100cceb5d26a7f3108f190c3de80d517
JoBICHE <jobiche.pro@gmail.com>
2022-01-17 17:02:18 +0100

    variables

---
+++ 1_Introduction/test.php
@@ -0,0 +1,32 @@
+
+
+<?php echo"<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+</head>" ?>
+
+<?php echo "<header></header>
+    <nav>
+        <li style=\"color:blue\">Test 1</li>
+        <li style='color:red'>Test 2</li>
+        <li style='color:green'>Test 3</li>
+    </nav>
+</header>" ?>
+
+<?php echo "<main>
+    <div>
+        <p style='font-size:3em'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque ea ad eius aspernatur ipsam autem aliquid nisi, saepe placeat voluptatum aperiam, quis, neque nobis eaque sapiente est quam maiores dolorem.</p>
+    </div>
+    <div>
+        <p>Ipsam porro ut voluptates laborum nobis. Ab adipisci corporis nobis autem magni porro cupiditate doloribus, voluptate alias obcaecati minus error, consequuntur unde earum? Earum qui aliquid nulla quisquam perferendis natus.</p>
+    </div>
+    <div>
+        <p>Nulla quia harum deleniti hic, qui illo in culpa officiis eligendi quaerat quod, est commodi. In voluptatem quaerat dignissimos perferendis architecto! Ex, placeat voluptates quo libero tempore doloremque numquam optio?</p>
+    </div>
+</main>" ?>
+
+<?php echo "<footer style='text-align:center'>Footer test</footer></html>"?>
--- 2_Variables/variables.php
+++ 2_Variables/variables.php
@@ -8,12 +8,33 @@
 </head>
 // Vos variables :
 <?php
-$nom = 'JO';
+ 
+$nom = 'Paul';
 $chiffre = 7;
 $nombre = 7 * 20;
+// Anciennce façon de déclarer un tableau :
+$personne = array(
+    "taille" => "192cm",
+    "genre" => "femme",
+    "travail" => "développeuse"
+);
+// Nouvelle et bonne façon de déclarer un tableau :
+$personne2 = [
+    1 => "192cm",
+    2 => "femme",
+    3 => "développeuse",
+    4 => 4
+];
+$personne3 = ['test', 'test2', 'test3'];
+
+// Les constantes :
+
+define('temperature',"100");
+
 ?>
 <body>
-    
+
+<?= $lorem ?>
     <h1>Exercice 1 : Créer puis manipuler des variables en PHP :</h1>
     
     <!--     Remplacez les XXX ci-dessous par des valeurs contenues dans des variables.
@@ -29,11 +50,29 @@ $nombre = 7 * 20;
 
     <p>Cette information ne présente aucun intéret, mais si vous vous posez la question, sachez que mon chiffre prote bonheur multiplié par 20 donne <?= $nombre ?></p>
 
+    <?php 
 
-    
+    $nom = 'Tim';
 
+    ?>
 
-    
-    
+    <p style="color: red;">Bonjour, en fait, je m'appelle <?= $nom ?>, j'ai XXX ans et je suis XXX de profession.</p>
+
+    <?= $nom ?>
+  
+<!-- Les types de variables : -->
+    <p>Le type de la variable est <?= gettype($nom) ?></p> 
+    <p>Le type de la variable est <?= gettype($chiffre) ?></p> 
+<?php var_dump($personne) ?>
+<?php echo nl2br("\n"); ?> <!--Saut de ligne-->
+<?php var_dump($personne2) ?>
+<?php echo $personne ?>
+<?php var_dump($personne2[3]); ?>
+<?php echo nl2br("\n"); ?> <!--Saut de ligne-->
+<p>Je mesure <?echo($personne["taille"]) ?></p>
+<?php echo nl2br("\n"); ?> <!--Saut de ligne-->
+<?php echo($personne3[0]); ?>
+<?= temperature; ?>
+<?php echo temperature ?>
 </body>
 </html>
